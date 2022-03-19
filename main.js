a5da70f077e2e769181638da36e70fedc5005bde
AimeeR74655 <82007449+AimeeR74655@users.noreply.github.com>
2022-03-19 18:47:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/dcPdemAdu/model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/DTj9VM-OM/model.json', modelReady);
 
 }
 
@@ -10,3 +10,60 @@ function modelReady()
     classifier.classify(gotResults);
 }
 
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error(error)
+    }
+    else
+    {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255)+1;
+        random_number_g = Math.floor(Math.random() * 255)+1;
+        random_number_b = Math.floor(Math.random() * 255)+1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label; 
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %"; 
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")"; 
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+        img = document.getElementById('alien1');
+        img1= document.getElementById('alien2');
+        img2 = document.getElementById('alien3');
+        img3 = document.getElementById('alien4');
+
+        if(results[0].label == "Background Noise")
+        {
+            img.src="background_noise.gif";
+            img1.src = "cat.png";
+            img2.src = "cow.png";
+            img3.src = "dog.png";
+        }
+       else if(results[0].label == "Meowing")
+       {
+        img.src = "background_noise.png";
+        img1.src = "cat_gif.gif";
+        img2.src = "cow.png";
+        img3.src = "dog.png";
+
+       }
+
+    else if(results[0].label == "Mooing")
+    {
+        img.src = "background_noise.png";
+        img1.src = "cat.png";
+        img2.src = "cow_gif.gif";
+        img3.src = "dog.png";
+    }
+    else
+    {
+        img.src = "background_noise.png";
+        img1.src = "cat.png";
+        img2.src = "cow.png";
+        img3.src = "dog_gif.gif";
+
+    }
+
+    }
+}
